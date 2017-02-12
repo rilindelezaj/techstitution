f7077a780bb6b2afa3e09f198e149b81ae6ec69b
Adelina Hajrizi <lina.hajrizi@hotmail.com>
2017-02-12 11:00:48 -0500

    files changed

--- app/mod_main/views.py
+++ app/mod_main/views.py
@@ -1,21 +1,49 @@
 from flask import Blueprint, render_template, request
 from app import mongo
-
+from bson import ObjectId
 mod_main = Blueprint('main', __name__)
+
 
 @mod_main.route('/')
 def index():
-	db = mongo.db.arkep
-	db.insert({"name" : "arkep"})
+
 	return render_template("index.html")
 
 @mod_main.route('/form', methods=['GET','POST'])
 def form():
+	db = mongo.db.arkep
 	if request.method == 'GET':
-		return render_template("form.html")
+		emri = "Filan Fistek Filani"
+		return render_template("form.html", emri=emri)
 	elif request.method == 'POST':
+		form_data = request.form.to_dict()
+		data = {
+          "nderrmarja":{
+            "emri": form_data['emri_ndermarrjes'],
+            "numri_regjistrimi":form_data['nr_regjistrimit'],
+            "adresa":form_data['adresa'],
+            "personi_kontaktues":form_data['personi_kontaktues'],
+            "telefoni":form_data['telefoni'],
+            "email":form_data['email']
+            }
+        }
+
 		db = mongo.db.arkep
-		db.insert({"name" : "arkep"})
-		return "Form inserted"
+		db.insert(data)
+		return render_template("form.html", mesazhi="Faleminderit, forma u insertua")
 	else:
 		return "Go home, you are drunk"
+
+@mod_main.route('/list', methods=['GET'])
+def list():
+	return render_template('list.html')
+
+@mod_main.route('/remove', methods=['POST'])
+def remove():
+	return render_template('list.html')
+
+@mod_main.route('/raporti/<string:report_id>')
+def raporti(report_id):
+	db = mongo.db.arkep
+	report = db.find_one({ "_id" : ObjectId(report_id) })
+	return render_template('raporti.html',report=report)
